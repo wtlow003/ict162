6faf10477848a899da84694ea852e350c8dc9325
wtlow003 <wtlow003@suss.edu.sg>
2021-04-18 21:49:44 +0800

    Added in docstrings for methods

--- q4.py
+++ q4.py
@@ -1,9 +1,21 @@
 import tkinter as tk
-from tkinter import scrolledtext, IntVar, StringVar, ttk
+from tkinter import scrolledtext, StringVar, ttk
 
 
 class BookingManagementGUI:
+    """A class to represent the GUI of Booking Management System.
 
+    The GUI allows uses to interact with to add, remove, display respective
+    booking and staycation information.
+
+    Attributes:
+        bookings (list): A list to keep track of the bookings for staycation
+            that occured.
+
+    Example:
+        >>> BookingManagementGUI(staycations: dict)
+
+    """
     bookings = []
 
     def __init__(self, staycations: dict):
@@ -14,22 +26,49 @@ class BookingManagementGUI:
 
         self._win.geometry("560x375")
         self._win.resizable(False, False)
-        self._win.configure(bg="#ececec")
+        self._win.config(bg="#ececec")
 
         self.start_application()
         self._win.mainloop()
 
     @staticmethod
     def label_widget(master, width, text, row, col):
-        """
+        """Method not bound to the object, to create a label widget for the GUI.
+
+        Args:
+            master (tkinter.Frame): Rectangular area that groups the Label Widget
+            width (int): Width of the intended Label Widget
+            text (str): Text to be displayed for the Label Widget
+            row (int): On the `master` grid, which position on the row to be placed
+            col (int):  On the `master` grid, which position on the column to be placed
+
+        Returns:
+            label (tkinter.Label): Returns Label Widget that display text for the
+                Entry Widget associated with
+
         """
         label = tk.Label(
             master=master, width=width, text=text, bg="#ececec", anchor='w', padx=5)
         label.grid(row=row, column=col)
 
+        return label
+
     @staticmethod
     def entry_widget(master, width, textvar, row, col):
-        """
+        """Method not bound to the object, to create a entry widget for the GUI.
+
+        Args:
+            master (tkinter.Frame): Rectangular area that groups the Entry Widget
+            width (int): Width of the intended Entry Widget
+            textvar (tkinter.StringVar): Creating string variable as set and accessed
+                by the Entry Widget
+            row (int): On the `master` grid, which positon on the row to be placed
+            col (int): On the `master` grid, which position on the column to be placed
+
+        Returns:
+            entry (tkinter.Entry): Returns Entry Widget that accept single-line
+                text strings from users
+
         """
         entry = tk.Entry(master, width=width, borderwidth=0.5, highlightthickness=0,
                          disabledbackground="#ececec", textvariable=textvar)
@@ -38,9 +77,10 @@ class BookingManagementGUI:
         return entry
 
     def start_application(self):
-        """
-        """
+        """Method to ensure that when starting the application, all Entry Widgets
+            are disabled to prevent entry until radio buttons are pressed.
 
+        """
         entry_widgets = [self._staycation_code_left_ety,
                          self._staycation_code_right_ety,
                          self._customer_id_ety,
@@ -52,12 +92,21 @@ class BookingManagementGUI:
             e.config(state='disabled')
 
     def select_radio_btn(self):
+        """Method to toggle selected Entry Widgets on either side (left/right)
+            based on the radio button pressed (Bookings/Staycations).
+
+        The associated Entry Widgets are tied to the respective radio button's value,
+        `self._booking_btn` = '0' and `self._staycation_btn` = '1'. When either
+        buttons are selected, the opposite Entry Widgets (e.g., for '1'), '0' will be disabled and
+        greyed out.
+
         """
-        """
+        # Checking on which radio button is pressed.
         button_pressed = self._radio_var.get()
 
         radio_buttons_links = {'0': [self._staycation_code_left_ety, self._customer_id_ety],
-                               '1': [self._staycation_code_right_ety, self._hotel_name_ety, self._nights_ety, self._cost_ety]}
+                               '1': [self._staycation_code_right_ety, self._hotel_name_ety,
+                                     self._nights_ety, self._cost_ety]}
 
         if button_pressed == '0':
             for linked in radio_buttons_links['0']:
@@ -73,13 +122,14 @@ class BookingManagementGUI:
                 unlinked.config(state='disable')
 
     def create_widgets(self):
+        """Method to create all the widgets required in the booking management GUI
+
         """
-        """
+        # Set the general style used by widgets created from ttk
         style = ttk.Style()
         style.theme_use('aqua')
 
-        last_radio_btn_pressed = None
-
+        ##### Desigining top radio buttons #####
         top_left = tk.Frame(self._win)
         top_right = tk.Frame(self._win)
         top_left.place(x=0, y=0)
@@ -88,82 +138,95 @@ class BookingManagementGUI:
         self._radio_var = StringVar()
         self._radio_var.set('')     # Deselect both radio buttons first
         self._booking_btn = tk.Radiobutton(
-            top_left, text='Booking', variable=self._radio_var, value='0', bg="#ececec", command=self.select_radio_btn)
+            top_left, text='Booking', variable=self._radio_var,
+            value='0', bg="#ececec", command=self.select_radio_btn)
         self._staycation_btn = tk.Radiobutton(
-            top_right, text='Staycation', variable=self._radio_var, value='1', bg="#ececec", command=self.select_radio_btn)
+            top_right, text='Staycation', variable=self._radio_var,
+            value='1', bg="#ececec", command=self.select_radio_btn)
 
         self._booking_btn.grid(row=0, column=0, sticky="w")
         self._staycation_btn.grid(row=0, column=0, sticky="w")
 
-        # Designing middle left entry text boxes
+        ##### Designing middle left entry text boxes #####
         middle_upper_left = tk.Frame(self._win)
         middle_upper_left.place(x=0, y=65)
         middle_upper_left.configure(bg="#ececec")
 
         # Label and entry widget for Staycation Code
-        self.label_widget(middle_upper_left, 11, 'Staycation Code:', 0, 0)
+        self._staycation_code_left_lbl = self.label_widget(
+            middle_upper_left, 11, 'Staycation Code:', 0, 0)
         self._staycation_code_left = StringVar()
         self._staycation_code_left_ety = self.entry_widget(
             middle_upper_left, 17, self._staycation_code_left, 0, 1)
 
         # Label and entry widget for Customer Id
-        self.label_widget(middle_upper_left, 11, 'Customer Id:', 1, 0)
+        self._customer_id_lbl = self.label_widget(middle_upper_left, 11, 'Customer Id:', 1, 0)
         self._customer_id = StringVar()
         self._customer_id_ety = self.entry_widget(middle_upper_left, 17, self._customer_id, 1, 1)
 
-        # Designing middle right entry text boxes
+        ##### Designing middle right entry text boxes #####
         middle_upper_right = tk.Frame(self._win)
         middle_upper_right.place(x=280, y=40)
         middle_upper_right.configure(bg="#ececec")
 
         # Label and entry widget for Staycation Code
-        self.label_widget(middle_upper_right, 11, 'Staycation Code:', 0, 0)
+        self._staycation_code_right_lbl = self.label_widget(
+            middle_upper_right, 11, 'Staycation Code:', 0, 0)
         self._staycation_code_right = StringVar()
         self._staycation_code_right_ety = self.entry_widget(
             middle_upper_right, 17, self._staycation_code_right, 0, 1)
 
         # Label and entry widget for Hotel Name
-        self.label_widget(middle_upper_right, 11, 'Hotel Name:', 1, 0)
+        self._hotel_name_lbl = self.label_widget(middle_upper_right, 11, 'Hotel Name:', 1, 0)
         self._hotel_name = StringVar()
         self._hotel_name_ety = self.entry_widget(middle_upper_right, 17, self._hotel_name, 1, 1)
 
         # Label and entry widget for Nights
-        self.label_widget(middle_upper_right, 11, 'Nights:', 2, 0)
+        self._nights_lbl = self.label_widget(middle_upper_right, 11, 'Nights:', 2, 0)
         self._nights = StringVar()
         self._nights_ety = self.entry_widget(middle_upper_right, 17, self._nights, 2, 1)
 
         # Label and entry widget for Cost
-        self.label_widget(middle_upper_right, 11, 'Cost: $', 3, 0)
+        self._cost_lbl = self.label_widget(middle_upper_right, 11, 'Cost: $', 3, 0)
         self._cost = StringVar()
         self._cost_ety = self.entry_widget(middle_upper_right, 17, self._cost, 3, 1)
 
-        # Designing middle buttons
+        ##### Designing middle buttons #####
         middle_lower = tk.Frame(self._win)
         middle_lower.place(x=125, y=160)
 
-        self._add_btn = ttk.Button(middle_lower, width=8, text='Add')
-        self._add_btn.bind("<Button-1>", self.add_button)
-        self._remove_btn = ttk.Button(middle_lower, width=8, text='Remove')
-        self._remove_btn.bind("<Button-1>", self.remove_button)
-        self._display_btn = ttk.Button(middle_lower, width=8, text='Display')
-        self._display_btn.bind("<Button-1>", self.display_button)
+        # Creating Add, Remove, Remove buttons and binding them to left mouse click
+        self._add_btn = ttk.Button(middle_lower, width=8, text='Add', command=self.add_button)
+        self._remove_btn = ttk.Button(
+            middle_lower, width=8, text='Remove', command=self.remove_button)
+        self._display_btn = ttk.Button(
+            middle_lower, width=8, text='Display', command=self.display_button)
+
+        # Placement of the buttons
         self._add_btn.pack(side='left')
         self._remove_btn.pack(side='left')
         self._display_btn.pack(side='left')
 
+        ##### Designing bottom scrolled text #####
         bottom = tk.Frame(self._win)
         bottom.place(x=5, y=187.5)
 
-        scrol_w, scrol_h = 77, 13
+        # Creating Scrolled Text to display the results given inputs.
+        scrol_w, scrol_h = 77, 13   # Setting scrolled text size
         self._scrolled_txt = scrolledtext.ScrolledText(
             bottom, width=scrol_w, height=scrol_h, wrap=tk.WORD, highlightthickness=0)
         self._scrolled_txt.grid(row=0, column=0, sticky="NSEW")
+        # Disable scrolled text to prevent users from typing at the start
+        # Only activating scrolled text when display of information is required
         self._scrolled_txt.config(state='disable')
 
     def check_entry(self, radio_button_pressed):
+        """
 
+        """
         radio_buttons_links = {'0': [self._staycation_code_left_ety, self._customer_id_ety],
-                               '1': [self._staycation_code_right_ety, self._hotel_name_ety, self._nights_ety, self._cost_ety]}
+                               '1': [self._staycation_code_right_ety, self._hotel_name_ety,
+                                     self._nights_ety, self._cost_ety]}
 
         empty = False
 
@@ -179,9 +242,12 @@ class BookingManagementGUI:
         return empty
 
     def clear_entry(self, radio_button_pressed):
+        """
 
+        """
         radio_buttons_links = {'0': [self._staycation_code_left_ety, self._customer_id_ety],
-                               '1': [self._staycation_code_right_ety, self._hotel_name_ety, self._nights_ety, self._cost_ety]}
+                               '1': [self._staycation_code_right_ety, self._hotel_name_ety,
+                                     self._nights_ety, self._cost_ety]}
 
         if radio_button_pressed == '0':
             for entry in radio_buttons_links['0']:
@@ -190,8 +256,10 @@ class BookingManagementGUI:
             for entry in radio_buttons_links['1']:
                 entry.delete(0, tk.END)
 
-    def add_button(self, event=None):
+    def add_button(self):
+        """
 
+        """
         # Enable editing of scrolledtext for program to input data
         self._scrolled_txt.config(state='normal')
         radio_button_pressed = self._radio_var.get()
@@ -234,8 +302,10 @@ class BookingManagementGUI:
         self._scrolled_txt.see(tk.END)
         self._scrolled_txt.config(state='disable')
 
-    def remove_button(self, event=None):
+    def remove_button(self):
+        """
 
+        """
         # Enable editing of scrolledtext for program to input data
         self._scrolled_txt.config(state='normal')
         radio_button_pressed = self._radio_var.get()
@@ -276,39 +346,45 @@ class BookingManagementGUI:
                             self.clear_entry(radio_button_pressed)
                             self._scrolled_txt.insert(tk.END, "Removed a staycation.\n")
                     except ValueError:
-                        self._scrolled_txt.insert(tk.END, "Nights or Cost field must be Integer.\n")
+                        self._scrolled_txt.insert(tk.END,
+                                                  "Nights or Cost field must be Integer.\n")
 
         self._scrolled_txt.see(tk.END)
         self._scrolled_txt.config(state='disable')
 
-    def display_button(self, event=None):
+    def display_button(self):
+        """
 
+        """
         # Enable editing of scolledtext of program to input data
         self._scrolled_txt.config(state='normal')
         radio_button_pressed = self._radio_var.get()
 
         if not radio_button_pressed:
-            self._scrolled_txt.insert(tk.END, "Select either Booking or Staycation first.\n")
+            self._scrolled_txt.insert(tk.END,
+                                      "Select either Booking or Staycation first.\n")
         elif radio_button_pressed == '0':
             if not type(self).bookings:
                 self._scrolled_txt.insert(tk.END, "No booking currently.\n")
             else:
                 text = '\n'.join(
-                    [f"Booking {idx + 1}: {val[0]} {val[1]}" for idx, val in enumerate(type(self).bookings)])
+                    [f"Booking {idx + 1}: {val[0]} {val[1]}"
+                     for idx, val in enumerate(type(self).bookings)])
                 self._scrolled_txt.insert(tk.END, text + '\n')
         else:
             if not self._staycations:
                 self._scrolled_txt.insert(tk.END, "No staycations currently.\n")
             else:
                 text = '\n'.join(
-                    [f"Staycation code: {key} {val[0]} {val[1]} nights ${val[2]}" for key, val in self._staycations.items()])
+                    [f"Staycation code: {key} {val[0]} {val[1]} nights ${val[2]}"
+                     for key, val in self._staycations.items()])
                 self._scrolled_txt.insert(tk.END, text + '\n')
 
         self._scrolled_txt.see(tk.END)
         self._scrolled_txt.config(state='disable')
 
 
-if __name__ == '__main__':
+def main():
     staycations = {'GM1': ['Grand Marina', 1, 238],
                    'GM2': ['Grand Marina', 2, 398],
                    'HB1': ['Hotel Bugis', 1, 168],
@@ -316,3 +392,7 @@ if __name__ == '__main__':
                    'HB3': ['Hotel Bugis', 3, 400]}
 
     BookingManagementGUI(staycations)
+
+
+if __name__ == '__main__':
+    main()
