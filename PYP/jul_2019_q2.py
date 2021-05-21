073685c5a371c2fdcb4faaa9ccc25fa4b9b597cf
wtlow003 <wtlow003@suss.edu.sg>
2021-05-21 22:52:56 +0800

    Completed instance methods for `Expenditure`

--- PYP/jul_2019_q2.py
+++ PYP/jul_2019_q2.py
@@ -1,10 +1,12 @@
 # Q2
+from datetime import datetime
 
 class ExpenditureException(Exception):
     """Exception class for exception raised for Expenditure"""
 
     def __init__(self, message, error_type):
-        self.error_type = error_type
+        super().__init__(message)
+        self._error_type = error_type
 
     @property
     def error_type(self):
@@ -16,25 +18,36 @@ class Expenditure:
     """
 
     def __init__(self, expenditure_date, amount, expenditure_type):
-        self._expenditure_date = expenditure_date
-        self._amount = amount
         self._expenditure_type = expenditure_type
+        # initialising `_amount`
+        if amount < 0:
+            raise ExpenditureException(f"Amount ${amount} cannot be negative.", 'Amount')
+        elif amount == 0:
+            raise ExpenditureException(f"Amount cannot be zero.", 'Amount')
+        else:
+            self._amount = amount
+        # initialising `_expenditure_date`
+        if expenditure_date > datetime.now():
+            formatted_date = expenditure_date.strftime('%a, %d %b %Y')
+            raise ExpenditureException(f"{formatted_date} cannot be later than today.", 'Date')
+        else:
+            self._expenditure_date = expenditure_date
 
     @property
     def expenditure_date(self):
-        pass
+        return self._expenditure_date
 
     @property
     def amount(self):
-        pass
+        return self._amount
 
     @property
     def expenditure_type(self):
-        pass
+        return self._expenditure_type
 
     def __str__(self):
         return (f"${self._amount:.2f} "
-                f"{self._expenditure_date} "
+                f"{self._expenditure_date.strftime('%a, %d %b %Y')} "
                 f"{self._expenditure_type}")
 
 
@@ -64,4 +77,26 @@ class ExpenditureList:
         pass
 
 if __name__ == '__main__':
-    pass
+    # testing Expenditure
+    e1 = Expenditure(datetime(2019, 8, 14), 125, 'Food')
+    e2 = Expenditure(datetime(2019, 8, 13), 4.50, 'Food')
+
+    try:
+        # testing raising errors in `Expenditure`
+        e3 = Expenditure(datetime(2021, 8, 20), 300, 'Food')
+    except ExpenditureException as e:
+        print(f"{e.error_type} Error: {e}")
+
+    # catch amount error - < 0
+    try:
+        e4 = Expenditure(datetime(2020, 5, 20), -10, 'Food')
+    except ExpenditureException as e:
+        print(f"{e.error_type} Error: {e}")
+
+    # catch amount error - == 0
+    try:
+        e5 = Expenditure(datetime(2020, 5, 20), 0, 'Food')
+    except ExpenditureException as e:
+        print(f"{e.error_type} Error: {e}")
+
+    print(e1, e2, sep='\n')
